fa54f05e30a522f59a1d983b9b002f7a95cf204a
Barbara Ristau <ristaub@gmail.com>
2017-01-14 15:21:04 -0800

    Added changes to UI

---
+++ tumblR/PhotoDetailsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoDetailsViewController.swift
+//  tumblR
+//
+//  Created by Barbara Ristau on 1/13/17.
+//  Copyright © 2017 FeiLabs. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+  
+  
+  var photoUrl: URL!
+  
+  // consider to delete the summary Label, need to work on passing data 
+  @IBOutlet weak var summaryLabel: UILabel!
+  
+  @IBOutlet weak var photoImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
